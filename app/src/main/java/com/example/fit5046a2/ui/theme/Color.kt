fa49fceb475b8487c786a89ed588e2165835f0ef
Administrator <1246978261@qq.com>
2026-04-22 09:53:44 +1000

    FIX

--- app/src/main/java/com/example/fit5046a2/ui/theme/Color.kt
+++ app/src/main/java/com/example/fit5046a2/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-// 主色调：医疗蓝，代表专业与信任
+// 主色调：医疗蓝
 val PrimaryBlue = Color(0xFF0288D1)
 val PrimaryBlueLight = Color(0xFF81D4FA)
 val PrimaryBlueDark = Color(0xFF01579B)
