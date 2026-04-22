7e784a22dd9f756d364071d434ac7e3d14a4f7a2
Administrator <1246978261@qq.com>
2026-04-22 10:18:19 +1000

    FIX

--- app/src/main/java/com/example/fit5046a2/navigation/NavGraph.kt
+++ app/src/main/java/com/example/fit5046a2/navigation/NavGraph.kt
@@ -1,9 +1,37 @@
 package com.example.fit5046a2.navigation
 
-object NavRoutes {
-    const val LOGIN = "login"
-    const val REGISTER = "register"
-    const val DASHBOARD = "dashboard"
-    const val HISTORY = "history"
-}
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.fit5046a2.screens.DashboardScreen
+
+
+@Composable
+fun CardioGuardNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.LOGIN // App 启动时首先进入登录页
+    ) {
+        composable(NavRoutes.LOGIN) {
+            // LoginScreen
+        }
+        composable(NavRoutes.REGISTER) {
+            //  RegisterScreen
+        }
+        composable(NavRoutes.DASHBOARD) {
+            // DashboardScreen
+            DashboardScreen(
+                onNavigateToHistory = {
+                    navController.navigate(NavRoutes.HISTORY)
+                }
+            )
+        }
+        composable(NavRoutes.HISTORY) {
+            //  HistoryScreen
+        }
+    }
+}
--- app/src/main/java/com/example/fit5046a2/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/fit5046a2/navigation/NavRoutes.kt
@@ -1,37 +1,8 @@
 package com.example.fit5046a2.navigation
 
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.example.fit5046a2.screens.DashboardScreen
-
-
-@Composable
-fun CardioGuardNavGraph(
-    navController: NavHostController
-) {
-    NavHost(
-        navController = navController,
-        startDestination = NavRoutes.LOGIN // App 启动时首先进入登录页
-    ) {
-        composable(NavRoutes.LOGIN) {
-            // LoginScreen
-        }
-        composable(NavRoutes.REGISTER) {
-            //  RegisterScreen
-        }
-        composable(NavRoutes.DASHBOARD) {
-            // DashboardScreen
-            DashboardScreen(
-                onNavigateToHistory = {
-                    navController.navigate(NavRoutes.HISTORY)
-                }
-            )
-        }
-        composable(NavRoutes.HISTORY) {
-            //  HistoryScreen
-        }
-    }
+object NavRoutes {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val DASHBOARD = "dashboard"
+    const val HISTORY = "history"
 }
